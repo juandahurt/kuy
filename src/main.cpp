5f02d8b45ddca1429aa94ace0c6f1c435fe3b323
Juan David Hurtado <juandahurtadom@gmail.com>
2023-12-15 21:34:53 -0500

    Add build settings

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,24 @@
+#include "./sfml/include/SFML/Graphics.hpp"
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    sf::RenderWindow window(sf::VideoMode(500, 500), "");
+    window.setFramerateLimit(60);
+    sf::CircleShape circle(30, 70);
+
+    circle.setPosition(50, 50);
+    circle.setFillColor(sf::Color(100, 200, 30));
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+        window.clear();
+        window.draw(circle);
+        window.display();
+    }
+    
     return 0;
 }
