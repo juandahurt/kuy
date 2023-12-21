f13af42663e4af14a050da93985627054ee5fec8
Juan David Hurtado <juandahurtadom@gmail.com>
2023-12-21 18:08:39 -0500

    Add basic game behavior

--- src/Game.cpp
+++ src/Game.cpp
@@ -4,9 +4,9 @@
 
 #include "Game.h"
 
+#include <cstdlib>
+
 Game::Game() {
-    _running = true;
-    _paused = false;
     _init();
 }
 
@@ -32,9 +32,13 @@ void Game::_spawnPlayer() {
 
 void Game::run() {
     while (_running) {
+        _entityManager.update();
         _sUserInput();
+        _sEnemySpawner();
         _sMovement();
+        _sCollision();
         _sRender();
+        _frameCount++;
     }
 }
 
@@ -97,10 +101,83 @@ void Game::_sUserInput() {
                     _player->cTransform->vel.x = 0;
                 }
                 break;
+            case sf::Event::MouseButtonPressed:
+                _spawnBullet(_player, Vec2((float) event.mouseButton.x, (float) event.mouseButton.y));
+                break;
         }
     }
 }
 
+void Game::_spawnBullet(std::shared_ptr<Entity> entity, Vec2 target) {
+    std::string tag = "bullet";
+    auto bullet = _entityManager.addEntity(tag);
+    // transform component
+    auto pos = entity->cTransform->pos;
+    auto vel = target - pos;
+    vel.normalize();
+    vel *= 2; // TODO: set proper velocity constant
+    bullet->cTransform = std::make_shared<CTransform>(Vec2(pos.x, pos.y), vel, 0);
+    auto bgColor = sf::Color(255, 255, 255);
+    auto outlineColor = sf::Color(100, 100, 200, 0);
+    // shape component
+    bullet->cShape = std::make_shared<CShape>(10.0f, 5, bgColor, outlineColor, 2.0f);
+    // lifespan component
+    bullet->cLifespan = std::make_shared<CLifespan>(90);
+}
+
 void Game::_sMovement() {
     _player->cTransform->pos += _player->cTransform->vel;
+    std::string enemyTag = "enemy";
+    for (auto &e : _entityManager.entities(enemyTag)) {
+        if (e->cTransform == nullptr) continue;
+        e->cTransform->pos += e->cTransform->vel;
+    }
+    std::string bulletTag = "bullet";
+    for (auto &e : _entityManager.entities(bulletTag)) {
+        if (e->cTransform == nullptr) continue;
+        e->cTransform->pos += e->cTransform->vel;
+        if (e->cLifespan == nullptr) continue;
+        auto color = e->cShape->circle.getFillColor();
+        color.a = ((float) e->cLifespan->remaining) / (float) e->cLifespan->total * 255.f;
+        e->cShape->circle.setFillColor(color);
+        if (e->cLifespan->remaining > 0) e->cLifespan->remaining--;
+        else e->destroy();
+    }
+}
+
+void Game::_sEnemySpawner() {
+    if (_frameCount - _lastFrameEnemySpawned == 60) {
+        _lastFrameEnemySpawned = _frameCount;
+        _spawnEnemy();
+    }
+}
+
+void Game::_spawnEnemy() {
+    std::string tag = "enemy";
+    auto entity = _entityManager.addEntity(tag);
+    int radius = 35;
+    auto randPoints = rand() % (6 + 1);
+    auto randX = rand() % (500 + 1 - radius);
+    auto randY = rand() % (500 + 1 - radius);
+    // transform component
+    entity->cTransform = std::make_shared<CTransform>(Vec2(randX, randY), Vec2(1, 2.3), 5);
+    auto bgColor = sf::Color(100, 150, 100);
+    auto outlineColor = sf::Color(100, 100, 200);
+    // shape component
+    entity->cShape = std::make_shared<CShape>(10.0f, randPoints, bgColor, outlineColor, 2.0f);
+}
+
+void Game::_sCollision() {
+    std::string enemyTag = "enemy";
+    // iterate through all enemies
+    for (auto &e : _entityManager.entities(enemyTag)) {
+        if (e->cTransform == nullptr) continue;
+        auto pos = e->cTransform->pos;
+        if (pos.x > 500 || pos.x < 0) {
+            e->cTransform->vel.x *= -1;
+        }
+        if (pos.y < 0 || pos.y > 500) {
+            e->cTransform->vel.y *= -1;
+        }
+    }
 }
--- src/Game.h
+++ src/Game.h
@@ -11,17 +11,20 @@
 
 class Game {
     sf::RenderWindow            _window;
-    bool                        _running;
-    bool                        _paused;
+    bool                        _running                = true;
+    bool                        _paused                 = false;
     std::shared_ptr<Entity>     _player;
     EntityManager               _entityManager;
+    int                         _frameCount             = 0;
+    int                         _lastFrameEnemySpawned  = 0;
 
     void _init();
     void _spawnPlayer();
-    void _spawnBullet();
+    void _spawnBullet(std::shared_ptr<Entity> entity, Vec2 target);
     void _sMovement();
     void _sUserInput();
     void _sRender();
+    void _spawnEnemy();
     void _sEnemySpawner();
     void _sCollision();
 public:
